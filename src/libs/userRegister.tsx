ac9c967bd9da63bcab1184dcae743fb1e553cb6b
Nattapatque <nattapatque@gmail.com>
2024-03-26 18:26:42 +0700

    edit backend_URL

--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -1,11 +1,11 @@
 export default async function userRegister(userName:string, userTel: string, userEmail:string, userPassword:string, userRole:string) {
     try {
-        const response =await fetch(`${process.env.BACKEND}/api/v1/auth/register`,{
+        const response =await fetch(`${process.env.BACKEND_URL}/api/v1/auth/register`,{
             method: "POST",
-            mode: "no-cors",
+            // mode: "no-cors",
             headers: {
                 "Content-Type": "application/json" ,
-                "Access-Control-Allow-Origin": "*"
+                // "Access-Control-Allow-Origin": "*"
             },
             body: JSON.stringify({
                 name: userName,
