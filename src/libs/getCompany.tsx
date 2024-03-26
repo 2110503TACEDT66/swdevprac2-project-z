796e93302e49952905238b75744206f99d29a25d
Nattapatque <nattapatque@gmail.com>
2024-03-26 17:48:27 +0700

    for deploy

--- src/libs/getCompany.tsx
+++ src/libs/getCompany.tsx
@@ -1,6 +1,6 @@
 
 export default async function getCompany(id:string) {
-    const response =await fetch(`https://presentation-day-1-z.vercel.app/api/v1/companies/${id}`)
+    const response =await fetch(`${process.env.BACKEND_URL}/api/v1/companies/${id}`)
     if(!response.ok){
         throw new Error("Failed to fetch hospital")
     }
